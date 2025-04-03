3464597244aab48ea8c2b761ed359d93535eb6b2
Henry Brinkman <Henry.Brinkman@topicus.nl>
2025-04-03 10:23:54 +0200

    Ports en run configs

--- react-remote/apps/react-remote/vite.config.ts
+++ react-remote/apps/react-remote/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/react-remote',
   server: {
-    port: 4200,
+    port: 4201,
     host: 'localhost',
   },
   preview: {
